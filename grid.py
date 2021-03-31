778922cd2b375879744792e2f64c8d966b3fe384
Vibhu rathore <79627056+Vibhurathore09@users.noreply.github.com>
2021-03-31 10:50:59 +0530

    use of grid

---
+++ grid.py
@@ -0,0 +1,11 @@
+from tkinter import *
+r = Tk()
+# Creating a Label Widget
+mylaebl1 = Label(r,text="hello world").grid(row =0, column = 0)
+mylaebl2 = Label(r,text="my name is Vibhu Rathore").grid(row =1,column = 5)
+mylaebl3 = Label(r, text="                         ").grid(row=1,column = 1)
+# Shoving it onto screen
+ 
+
+
+r.mainloop()
